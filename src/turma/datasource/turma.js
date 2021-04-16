3a458f3e1b6a4ff844f943097f41ec5344475cda
Williams25 <william.souza.timao@hotmail.com>
2021-04-16 16:56:32 -0300

    Implementando schemas para turmas

--- src/turma/datasource/turma.js
+++ src/turma/datasource/turma.js
@@ -20,6 +20,38 @@ class Turma extends SQLDataSource {
     const turma = await this.knex.select("*").from("turmas").where("id", id);
     return turma[0];
   }
+
+  async incluiTurma(novaTurma) {
+    const novaTurmaId = await this.knex
+      .insert(novaTurma)
+      .returning('id')
+      .into('turmas')
+
+    const turmaInserida = await this.getTurma(novaTurmaId[0])
+    return ({ ...turmaInserida })
+  }
+
+  async atualizaTurma(novosDados) {
+    await this.knex
+      .update({ ...novosDados.turma })
+      .where({ id: Number(novosDados.id) })
+      .into('turmas')
+
+    const turmaAtualizada = await this.getTurma(novosDados.id)
+    return ({
+      ...turmaAtualizada
+    })
+  }
+
+  async deletaTurma(id) {
+    await this.knex('turmas')
+      .where({ id: id })
+      .del()
+
+    this.Resposta.mensagem = "registro deletado"
+    return this._setStatusAndMenssagem(200, "Operação efetuada com sucesso")
+  }
+
 }
 
 module.exports = Turma;
