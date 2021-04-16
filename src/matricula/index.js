a3a1c4c4ca731fee17afac724152435315a595cb
Williams25 <william.souza.timao@hotmail.com>
2021-04-16 16:54:54 -0300

    Ponto de entrada para matriculas

--- src/matricula/index.js
+++ src/matricula/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   matriculaSchema: require("./schema/matricula.graphql"),
-  matriculaResolver: require("./resolvers/matriculaResolvers")
+  matriculaResolver: require("./resolvers/matriculaResolvers"),
+  matriculaDataSource: require("./datasource/matricula")
 }
